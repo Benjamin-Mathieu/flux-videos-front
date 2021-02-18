e2e82a6f2f693cdb9fe9d48837ef7e016a95a931
Benjamin <ben88200@gmail.com>
2021-02-18 17:15:51 +0100

    Ajout de la carte leaflet

--- src/views/WatchStream.vue
+++ src/views/WatchStream.vue
@@ -1,20 +1,16 @@
 <template>
-    <div>
-        Salut
+    <div class="watch-stream">
+        <div class="stream"></div>
+        <div id="map"></div>
     </div>
 </template>
 
 <script>
-export default {
-    data()
-    {
-        return{
-            id:''
-        }
-    },
-    methods: {
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-    },
+
+export default {
     mounted()
     {
         /*Si pas de room existante avec
@@ -39,6 +35,12 @@ export default {
             };
             connection.socketMessageEvent = 'screen-sharing';
 
+            connection.onstream = function(event) {
+                let video = event.mediaElement;
+                let stream = document.querySelector(".stream").appendChild(video);
+                stream.classList.add("stream");
+            }; 
+
 
             (function reCheckRoomPresence() {
                 connection.checkPresence(roomid, function(isRoomExist) {
@@ -46,10 +48,50 @@ export default {
                         connection.join(roomid);
                     }
 
-                    //setTimeout(reCheckRoomPresence, 5000);
+                    // setTimeout(reCheckRoomPresence, 5000);
                 });
             })();
+            
+// Ajout de la carte Leaflet pour le stream
+            api.get("stream/" + roomid)
+            .then(response => {
+                let latitude = response.data.latitude;
+                let longitude = response.data.longitude;
+
+                let map = L.map("map").setView([latitude, longitude], 15);
+            
+                let openStreetMapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '© OpenStreetMap contributors',
+                    maxZoom: 50
+                    });
+
+                let marker = L.marker([latitude, longitude]).addTo(map);
+                marker.bindPopup("<b>Vous êtes ici !</b>").openPopup();
+                map.addLayer(openStreetMapLayer);
+
+            }).catch(error=>
+            {
+                console.log("error")
+            })
         }
+
+        
     }
 }
 </script>
+
+<style lang="scss">
+
+    .watch-stream {
+        background-color: red;
+        .stream {
+            width: 80%;
+            margin: auto;
+        }
+        #map {
+            height: 400px; width: 400px;
+        }
+
+    }
+    
+</style>
